3e77dedda4bc87c063a61851b1e0772c44495fc3
Justin Good <jgoodhcg@gmail.com>
2019-03-22 20:42:42 -0400

    Fix borked change detection on form

--- src/main/time_align_mobile/components/form_fields.cljs
+++ src/main/time_align_mobile/components/form_fields.cljs
@@ -2,8 +2,10 @@
   (:require [time-align-mobile.styles :refer [field-label-changeable-style
                                               field-label-style]]
             [re-frame.core :refer [dispatch]]
+            [reagent.core :as r :refer [atom]]
             ["react" :as react]
             ["react-native-elements" :as rne]
+            ["react-native" :as rn]
             [time-align-mobile.js-imports :refer [view
                                                   text
                                                   text-input
@@ -37,6 +39,7 @@
 (defn label-comp [form changes update-key]
   [view {:style field-style}
    [:> rne/Input {:label          "Label"
+                  :label-style    (field-label-changeable-style changes :label)
                   :default-value  (:label @form)
                   :spell-check    true
                   :on-change-text (fn [text]
@@ -52,27 +55,36 @@
                      :update update-structured-data}]])
 
 (defn parent-id-comp [form changes]
-  [view {:style {:flex-direction "row"}}
-   [text {:style (field-label-changeable-style @changes :bucket-id)}
-    ":bucket-id"]
-   [text (str (:bucket-id @form))]])
+  [view {:style field-style}
+   [:> rne/Input {:label          "Bucket ID"
+                  :label-style    (field-label-changeable-style @changes :bucket-id)
+                  :value  (str (:bucket-id @form))
+                  :editable false}]])
 
 (defn parent-picker-comp [form changes buckets update-key]
-  [view {:style {:flex-direction "row"
-                 :align-items "center"}}
-   [text {:style (field-label-changeable-style @changes :bucket-label)}
-    ":bucket-label"]
-   [picker {:selected-value  (:bucket-id @form)
-            :style           {:width 250}
-            :on-value-change #(dispatch [update-key {:bucket-id %}])}
+  [view {:style (merge field-style {:flex-direction  "column"
+                                    :padding         8
+                                    :justify-content "flex-start"
+                                    :align-items     "flex-start"})}
+   [:> rne/Text {:h4 true :h4-style
+                 (merge
+                  (field-label-changeable-style @changes :bucket-label)
+                  {:font-size 16})} "Bucket Label"]
+   [:> rn/Picker {:selected-value  (str (:bucket-id @form))
+                  :style           {:width 250}
+                  :on-value-change #(dispatch [update-key {:bucket-id %}])}
     (map (fn [bucket] [picker-item {:label (:label bucket)
-                                    :key (:id bucket)
+                                    :key   (:id bucket)
                                     :value (:id bucket)}])
          @buckets)]])
 
 (defn planned-comp [form changes update-key]
-  [view {:style {:flex-direction "row"
-                 :align-items    "center"}}
-   [text {:style (field-label-changeable-style @changes :planned)} ":planned"]
+  [view {:style (merge field-style {:flex-direction  "column"
+                                    :padding         8
+                                    :justify-content "flex-start"
+                                    :align-items     "flex-start"})}
+   [:> rne/Text {:h4 true :h4-style (merge
+                                     (field-label-changeable-style @changes :planned)
+                                     {:font-size 16})} "Planned"]
    [switch {:value (:planned @form)
             :on-value-change #(dispatch [update-key {:planned %}])}]])
--- src/main/time_align_mobile/styles.cljs
+++ src/main/time_align_mobile/styles.cljs
@@ -1,12 +1,21 @@
-(ns time-align-mobile.styles)
+(ns time-align-mobile.styles
+  (:require
+   ;; [cljs.spec.test.alpha :as stest]
+   ;; [cljs.spec.alpha :as s]
+   ))
 
+;; https://github.com/react-native-training/react-native-elements/blob/master/src/config/colors.js#L9
 (defn field-label-changeable-style [changes field]
-  {:color         (if (contains? changes field)
-                    "blue"
-                    "grey")
-   :width 80
-   :padding-right 5})
+  (if (contains? changes field)
+    {:color "#00ff00"}
+    {:color "#86939e"}))
 
+;; (s/fdef field-label-changeable-style
+;;   :args (s/and #(map? (first %))
+;;                #(keyword? (second %)))
+;;   :ret map?)
+
+;; (stest/instrument 'field-label-changeable-style)
 
 (def field-label-style {:color         "grey"
                         :padding-right 5
