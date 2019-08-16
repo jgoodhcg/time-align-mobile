8c57ac0060fbf80a87481b429a1cb360a452fdb0
Justin Good <jgoodhcg@gmail.com>
2019-08-16 19:13:23 -0400

    Getting closer

--- src/main/time_align_mobile/screens/calendar.cljs
+++ src/main/time_align_mobile/screens/calendar.cljs
@@ -21,34 +21,39 @@
 
 (def top (r/atom 0))
 
+(def selected (r/atom false))
+
 (defn root [params]
   (let [pan-ref        (.createRef react)
         pinch-ref      (.createRef react)
         double-tap-ref (.createRef react)]
-    [scroll-view-gesture-handler {:wait-for double-tap-ref
-                                  :on-gesture-event #(println "scroll gesture")
+    [scroll-view-gesture-handler {:wait-for                double-tap-ref
+                                  :on-gesture-event        #(println "scroll gesture")
                                   :on-handler-state-change #(println (str "scroll " (get-state %)))}
-     [tap-gesture-handler {:ref                     double-tap-ref
-                           :wait-for                pan-ref
-                           :on-handler-state-change #(println (str "tap " (get-state %)))
-                           :on-gesture-event        #(println "tap gesture")}
-      [view {:style {:flex             1
-                     :flex-direction   "column"
-                     :height           2000
-                     :background-color "grey"
-                     :justify-content  "space-between"
-                     :align-items      "center"}}
-       [pan-gesture-handler {:ref                     pan-ref
-                             :on-handler-state-change #(println (str "pan " (get-state %)))
-                             :on-gesture-event        #(println "pan gesture")}
+     [view {:style {:flex             1
+                    :flex-direction   "column"
+                    :height           2000
+                    :background-color "grey"
+                    :justify-content  "space-between"
+                    :align-items      "center"}}
+      [pan-gesture-handler {:ref                     pan-ref
+                            :enabled                 @selected
+                            :on-handler-state-change #(println (str "pan " (get-state %)))
+                            :on-gesture-event        #(println "pan gesture")}
+       [tap-gesture-handler {:ref                     double-tap-ref
+                             :wait-for                pan-ref
+                             :on-handler-state-change #(if (= (get-state %) "active")
+                                                         (do (println "tapped!")
+                                                             (swap! selected not))
+                                                         (println "not active tap state change"))}
 
-        [view {:style {:background-color "red"
+        [view {:style {:background-color (if @selected "purple" "green")
                        :position         "absolute"
                        :height           150
                        :width            150
                        :top              @top}}
-         [text "this is a calendar page"]]]
+         [text "this is a calendar page"]]]]
 
-        [text "here is a thing"]
+      [text "here is a thing"]
 
-        [text "here is another thing"]]]]))
+      [text "here is another thing"]]]))
