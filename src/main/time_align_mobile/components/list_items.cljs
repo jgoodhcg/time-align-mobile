0e0b4b026422a8e389e55422b4b42a9e932e0b8c
Justin Good <jgoodhcg@gmail.com>
2019-11-28 13:54:45 -0500

    Improve pattern list

--- src/main/time_align_mobile/components/list_items.cljs
+++ src/main/time_align_mobile/components/list_items.cljs
@@ -1,36 +1,27 @@
 (ns time-align-mobile.components.list-items
   (:require [time-align-mobile.js-imports :refer [view
-                                                  text
+                                                  text-paper
                                                   list-item
                                                   list-icon
                                                   touchable-highlight]]
+            [time-align-mobile.styles :refer [theme]]
             [reagent.core :as r]
             ["react" :as react]))
 
 
 (defn bucket [{:keys [id color label periods templates on-press]}]
-  [list-item {:title       (r/as-element [text label])
-              :key         (str id)
-              :on-press    on-press
-              :left        #(r/as-element
-                             [list-icon {:icon  "google-circles-communities"
-                                         :color color}])}]
-  ;; [touchable-highlight
-  ;;  {:key      id
-  ;;   :on-press on-press}
-
-  ;;  [view {:style {:flex-direction "row"}}
-  ;;   [view
-  ;;    {:style {:width            50
-  ;;             :height           50
-  ;;             :margin-right     20
-  ;;             :background-color color}}]
-  ;;   [view {:style {:flex-direction "column"}}
-  ;;    [text (if (> (count label) 0) label "No label")]
-  ;;    [text {:style {:color "grey"}} "periods: " (count periods)]
-  ;;    [text {:style {:color "grey"}} "templates: " (count templates)]
-  ;;    [text {:style {:color "grey"}} (str "id: " id)]]]]
-  )
+  [list-item {:title    (r/as-element
+                         (if (empty? label)
+                           [text-paper {:style
+                                        {:color      (-> theme :colors :disabled)
+                                         :font-style "italic"}}
+                            "Group has no label"]
+                           [text-paper label]))
+              :key      (str id)
+              :on-press on-press
+              :left     #(r/as-element
+                          [list-icon {:icon  "google-circles-communities"
+                                      :color color}])}])
 
 (defn bucket-key-extractor [x]
   (-> x
@@ -39,20 +30,17 @@
       (str)))
 
 (defn pattern [{:keys [id label templates on-press]}]
-  [touchable-highlight
-   {:key      id
-    :on-press on-press}
-
-   [view {:style {:flex-direction "row"}}
-    [view ;; TODO make this not a square
-     {:style {:width            50
-              :height           50
-              :margin-right     20
-              :background-color "grey"}}]
-    [view {:style {:flex-direction "column"}}
-     [text (if (> (count label) 0) label "No label")]
-     [text {:style {:color "grey"}} "templates: " (count templates)]
-     [text {:style {:color "grey"}} (str "id: " id)]]]])
+  [list-item {:title    (r/as-element
+                         (if (empty? label)
+                           [text-paper {:style
+                                        {:color      (-> theme :colors :disabled)
+                                         :font-style "italic"}}
+                            "Plan has no label"]
+                           [text-paper label]))
+              :key      (str id)
+              :on-press on-press
+              :left     #(r/as-element
+                          [list-icon {:icon "floor-plan"}])}])
 
 (defn template [{:keys [id color label pattern-label bucket-label on-press]}]
   [touchable-highlight
@@ -78,22 +66,22 @@
     [view {:style {:flex-direction "column"}}
 
      ;; label
-     [text (if (> (count label) 0)
+     [text-paper (if (> (count label) 0)
              label
              "No label")]
 
      ;; bucket label
-     [text {:style {:color "grey"}}
+     [text-paper {:style {:color "grey"}}
       (if (> (count bucket-label) 0)
         (str "bucket-label: " bucket-label)
         "No bucket label")]
 
-     [text {:style {:color "grey"}}
+     [text-paper {:style {:color "grey"}}
       (if (> (count pattern-label) 0)
         (str "pattern-label: " pattern-label)
         "No pattern label")]
      ;; id
-     [text {:style {:color "grey"}}
+     [text-paper {:style {:color "grey"}}
       (str "id: " id)]]]])
 
 ;; TODO move periods and filters
--- src/main/time_align_mobile/screens/pattern_list.cljs
+++ src/main/time_align_mobile/screens/pattern_list.cljs
@@ -1,12 +1,17 @@
 (ns time-align-mobile.screens.pattern-list
 (:require [time-align-mobile.js-imports :refer [view
-                                                  text
-                                                  flat-list
-                                                  touchable-highlight]]
+                                                text
+                                                flat-list
+                                                list-section
+                                                scroll-view
+                                                icon-button
+                                                subheading
+                                                touchable-highlight]]
             [time-align-mobile.components.filter-picker :refer [filter-picker
                                                                 filter-sort]]
             [time-align-mobile.components.list-buttons :as list-buttons]
             [time-align-mobile.components.list-items :as list-items]
+            [time-align-mobile.components.top-bar :refer [top-bar]]
             [reagent.core :as r :refer [atom]]
             ["react" :as react]
             ["react-native-elements" :as rne]
@@ -16,24 +21,23 @@
   (let [patterns      (subscribe [:get-patterns])
         active-filter (subscribe [:get-active-filter])]
 
-    [view {:style {:flex 1 :justify-content "center" :align-items "center"}}
-
-     [text "Patterns"]
-     [filter-picker :pattern]
-     [flat-list {:data          (filter-sort @patterns @active-filter)
-                 :key-extractor (fn [x] (-> x (js->clj) (get "id") (str)))
-                 :render-item
-                 (fn [i]
-                   (let [item (:item (js->clj i :keywordize-keys true))]
-                     (r/as-element
-                      (list-items/pattern
-                       (merge
-                        item
-                        {:on-press
-                         #(dispatch
-                           [:navigate-to
-                            {:current-screen :pattern
-                             :params         {:pattern-id (:id item)}}])})))))}]
+    [view {:style {:flex 1}}
+     [top-bar {:center-content [subheading "Plans"]
+               :right-content  [icon-button]}]
+     [scroll-view
+      [list-section
+       (->> (filter-sort @patterns @active-filter)
+            (map (fn [item]
+                   (println item)
+                   (r/as-element
+                    (list-items/pattern
+                     (merge
+                      item
+                      {:on-press
+                       #(dispatch
+                         [:navigate-to
+                          {:current-screen :pattern
+                           :params         {:pattern-id (:id item)}}])}))))))]]
 
      [list-buttons/root
       #(dispatch [:add-new-pattern {:id (random-uuid)
