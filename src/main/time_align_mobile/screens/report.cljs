972230e212dd87351ded0ddaf5af5fdd5af5d9a1
Justin Good <jgoodhcg@gmail.com>
2019-12-01 16:18:21 -0500

    Add top bar

--- src/main/time_align_mobile/screens/report.cljs
+++ src/main/time_align_mobile/screens/report.cljs
@@ -1,32 +1,38 @@
 (ns time-align-mobile.screens.report
   (:require [time-align-mobile.js-imports :refer [view
                                                   line-chart
+                                                  subheading
+                                                  icon-button
                                                   text]]
+            [time-align-mobile.components.top-bar :refer [top-bar]]
             ["react" :as react]))
 
 (defn root [params]
-  [view {:style {:flex 1 :justify-content "center" :align-items "center"}}
-   [line-chart
-    {:data
-     (clj->js {:labels   (clj->js ["A" "B" "C"])
-               :datasets (clj->js
-                          [(clj->js {:data (clj->js [2 5 7])})])})
-     :bezier        true
-     :width         400
-     :height        400
-     :y-axis-label  "$"
-     :y-axis-suffix "k"
-     :chart-config
-     (clj->js {:backgroundColor        "#e26a00"
-               :backgroundGradientFrom "#fb8c00"
-               :backgroundGradientTo   "#ffa726"
-               :labelColor
-               (clj->js
-                #(str "rgba(255, 255, 255, "
-                      (if-some [opacity %] opacity 1)
-                      ")"))
-               :color
-               (clj->js
-                #(str "rgba(255, 255, 255, "
-                      (if-some [opacity %] opacity 1)
-                      ")"))})}]])
+  [view {:style {:flex 1}}
+   [top-bar {:center-content [subheading "Reports"]
+             :right-content  [icon-button]}]
+   [view {:style {:flex 1
+                  :align-items "center"}}
+    [line-chart
+     {:data
+      (clj->js {:labels   (clj->js ["A" "B" "C"])
+                :datasets (clj->js
+                           [(clj->js {:data (clj->js [2 5 7])})])})
+      :width         400
+      :height        400
+      :y-axis-label  "$"
+      :y-axis-suffix "k"
+      :chart-config
+      (clj->js {:backgroundColor        "#e26a00"
+                :backgroundGradientFrom "#fb8c00"
+                :backgroundGradientTo   "#ffa726"
+                :labelColor
+                (clj->js
+                 #(str "rgba(255, 255, 255, "
+                       (if-some [opacity %] opacity 1)
+                       ")"))
+                :color
+                (clj->js
+                 #(str "rgba(255, 255, 255, "
+                       (if-some [opacity %] opacity 1)
+                       ")"))})}]]])
